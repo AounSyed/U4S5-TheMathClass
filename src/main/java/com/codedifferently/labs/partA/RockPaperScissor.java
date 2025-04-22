7858f10509dba245839840dd6fd973a33392d03a
AounSyed <Aoungalaxy@gmail.com>
2025-04-22 16:53:08 -0400

    worked

--- src/main/java/com/codedifferently/labs/partA/RockPaperScissor.java
+++ src/main/java/com/codedifferently/labs/partA/RockPaperScissor.java
@@ -1,9 +1,22 @@
 package com.codedifferently.labs.partA;
-
+import java.util.Random;
 public class RockPaperScissor {
 
     public static void main(String[] args) {
+        Random rand = new Random();
+        int number = rand.nextInt(3);
+        System.out.println(number);
 
+        if (number == 0)
+        {
+            System.out.println("Rock");
+        } else if (number == 1)
+        {
+            System.out.println("Paper");
+        }
+        else{
+            System.out.println("Scissors");
+        }
 
     }
 }
